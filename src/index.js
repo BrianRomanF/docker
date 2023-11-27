eda4571abe5ba5d5aefbf9bc956d389e0e0a92af
BrianRomanF <broman.89@gmail.com>
2023-11-27 20:12:10 +1300

    another one

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
 const app = require("express")();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8080;  
 const env = process.env.NODE_ENV; 
 
 app.get("/", (req, res) => {
