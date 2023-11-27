6c2c7e4bfcd502630f05b786d0d525e67cf588ca
BrianRomanF <broman.89@gmail.com>
2023-11-27 20:02:51 +1300

    First commit

---
+++ docker/src/index.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+
+const app = require("express")();
+const PORT = process.env.PORT || 8080;
+const env = process.env.NODE_ENV; 
+
+app.get("/", (req, res) => {
+  res.send(`Server is up and running! in ${env}`);
+});
+app.listen(PORT, () => {
+  console.log(`Listening to ${PORT}`);
+});
